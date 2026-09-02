8fac7f7c4680eaca3891c140a3c5e4941ade1e99
yunfei00 <goodman_yunfei@163.com>
2026-09-02 11:12:24 +0800

    feat: export DSO-X Snapshot All baseline API

--- packages/instrument_drivers/src/instrument_drivers/keysight/dsox3000/__init__.py
+++ packages/instrument_drivers/src/instrument_drivers/keysight/dsox3000/__init__.py
@@ -1,5 +1,11 @@
 from .driver import KeysightDSOX3000Driver
 from .single_capture import acquire_single_word_waveform
+from .snapshot import (
+    SNAPSHOT_ALL_MEASUREMENTS,
+    SnapshotMeasurementSpec,
+    parse_snapshot_value,
+    read_snapshot_all,
+)
 from .waveform import (
     DecodedWaveform,
     WaveformPreamble,
@@ -10,6 +16,10 @@ from .waveform import (
 __all__ = [
     "KeysightDSOX3000Driver",
     "acquire_single_word_waveform",
+    "SNAPSHOT_ALL_MEASUREMENTS",
+    "SnapshotMeasurementSpec",
+    "parse_snapshot_value",
+    "read_snapshot_all",
     "DecodedWaveform",
     "WaveformPreamble",
     "build_waveform",
